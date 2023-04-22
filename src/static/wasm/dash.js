97af78b552ecfd0b3736637372e7cb386b873515
Luis Ale Mendoza <lalexmf117@gmail.com>
2023-04-21 22:26:47 -0600

    Add user interaction with colors

--- src/static/wasm/dash.js
+++ src/static/wasm/dash.js
@@ -4,6 +4,7 @@ const btnStep = document.getElementById('btnStep');
 const caConfigForm = document.getElementById('ca-configForm')
 const caRulesForm = document.getElementById('ca-rulesForm')
 const caRulesList = document.getElementById('ca-rulesList')
+const caStatesColors = document.getElementById('ca-statesColors')
 
 const go = new Go();
 fetch('/static/wasm/main.wasm') // Path to the WebAssembly binary file
@@ -13,6 +14,7 @@ if (WebAssembly.validate(buffer)) {
 // Load the WebAssembly module
 WebAssembly.instantiate(buffer, go.importObject).then(result => {
 go.run(result.instance);
+    let statesColors = ["#000000", "#ffffff"]
     const randomMatrix = (width, height, numStates) => {
         let matrix = [];
         for (let i = 0; i < height; i++) {
@@ -43,8 +45,9 @@ go.run(result.instance);
                 cell.style.width = "20px"
                 cell.style.height = "20px"
                 cell.style.border = "1px solid black"
-                cell.style.backgroundColor = cellMatrix[i][j] == 0 ? 'black': 'white'
-                cell.style.color = cellMatrix[i][j] == 1 ? 'black': 'white'
+                console.log(cellMatrix[i][j])
+                console.log(statesColors)
+                cell.style.backgroundColor = statesColors[cellMatrix[i][j]]
                 //cell.textContent = cellMatrix[i][j]
                 row.appendChild(cell)
             }
@@ -78,7 +81,36 @@ go.run(result.instance);
         }
         caRulesList.appendChild(fragment)
     }
-
+    const loadStatesColorsOnUI = (statesColors) => {
+        while (caStatesColors.firstChild) {
+            caStatesColors.removeChild(caStatesColors.firstChild);
+        }
+        fragment = document.createDocumentFragment()
+        statesLabelRow = document.createElement('tr')
+        statesColorRow = document.createElement('tr')
+        for (let i = 0; i < statesColors.length; i++) {
+            const state = document.createElement('td')
+            const color = document.createElement('td')
+            const colorPicker = document.createElement('input')
+            state.textContent = i
+            colorPicker.setAttribute('type', 'color')
+            colorPicker.value = statesColors[i]
+            colorPicker.dataset.state = i
+            colorPicker.style.width = "20px"
+            colorPicker.style.height = "20px"
+            colorPicker.style.border = "1px solid black"
+            colorPicker.addEventListener('change', () => {
+                statesColors[colorPicker.dataset.state] = colorPicker.value
+                loadMatrixOnUI(cellMatrix)
+            })
+            color.appendChild(colorPicker)
+            statesLabelRow.appendChild(state)
+            statesColorRow.appendChild(color)
+        }
+        fragment.appendChild(statesLabelRow)
+        fragment.appendChild(statesColorRow)
+        caStatesColors.appendChild(fragment)
+    }            
     //////////////////
     let conf = {
         numStates: parseInt(caConfigForm.elements['ca-numStates'].value),
@@ -93,6 +125,7 @@ go.run(result.instance);
         Rule2d("0==0", 0)
     ]
     loadRulesOnUI(rules)
+    loadStatesColorsOnUI(statesColors)
     ca.setRules(rules)
     // Default a random matrix. TODO: Listen for user changes on UI
     err = ca.loadInitGrid(randomMatrix(conf.width, conf.height,conf.numStates))
